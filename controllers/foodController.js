4494a69b26736989c11c6da5b1408b08bae8eb7c
willzer0 <pembunuh4444@gmail.com>
2025-09-26 13:50:44 +0800

    refactor foodController

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -1,91 +1,62 @@
-const Food = require("../models/foodModel");
-const APIFeatures = require("../utils/APIFeatures");
-
-exports.getAllFoods = async (req, res) => {
-  try {
-    const features = new APIFeatures(Food.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
-    const foods = await features.query;
-    res.status(200).json({
-      status: "success",
-      results: foods.length,
-      data: {
-        foods,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err.message,
-    });
-  }
-};
-
-exports.createFood = async (req, res) => {
-  try {
-    const newFood = await Food.create(req.body);
-    res.status(201).json({
-      status: "success",
-      data: {
-        food: newFood,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err.message,
-    });
-  }
-};
-exports.getFood = async (req, res) => {
-  try {
-    const food = await Food.findById(req.params.id);
-    res.status(200).json({
-      status: "success",
-      data: {
-        food,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err.message,
-    });
-  }
-};
-exports.updateFood = async (req, res) => {
-  try {
-    const food = await Food.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    res.status(200).json({
-      status: "success",
-      data: {
-        food,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err.message,
-    });
-  }
-};
-exports.deleteFood = async (req, res) => {
-  try {
-    await Food.findByIdAndDelete(req.params.id);
-    res.status(204).json({
-      status: "success",
-      data: null,
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err.message,
-    });
-  }
-};
+const Food = require('../models/foodModel');
+const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+//////////////////////////////////
+exports.getAllFoods = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Food.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const foods = await features.query;
+  res.status(200).json({
+    status: 'success',
+    results: foods.length,
+    requestTime: req.requestTime,
+    data: {
+      foods,
+    },
+  });
+});
+//////////////////////////////////
+exports.createFood = catchAsync(async (req, res, next) => {
+  const newFood = await Food.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      food: newFood,
+    },
+  });
+});
+///////////////////////////////////
+exports.getFood = catchAsync(async (req, res, next) => {
+  const food = await Food.findById(req.params.id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      food,
+    },
+  });
+});
+////////////////////////////////////
+exports.updateFood = catchAsync(async (req, res, next) => {
+  const food = await Food.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      food,
+    },
+  });
+});
+//////////////////////////////////
+exports.deleteFood = catchAsync(async (req, res, next) => {
+  await Food.findByIdAndDelete(req.params.id);
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
