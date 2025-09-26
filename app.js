8585ea9e4bb73a19a9d376c690f01259769b39a3
willzer0 <pembunuh4444@gmail.com>
2025-09-26 13:28:02 +0800

    adding unhandled rejection error handling

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
-// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/foods', foodRouter);
 app.use('/api/v1/users', userRouter);
