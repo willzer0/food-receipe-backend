0ca0c6adce35127fa3b541b61cd5b7e2a94278c2
willzer0 <pembunuh4444@gmail.com>
2025-10-07 13:52:47 +0800

    adding error security in app

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const csurf = require('csurf');
 ////////////////////////////////
 const foodRouter = require('./routes/foodRouter');
 const userRouter = require('./routes/userRouter');
@@ -8,10 +14,41 @@ const userRouter = require('./routes/userRouter');
 const errorHandler = require('./controllers/errorController');
 const appError = require('./utils/appError');
 //////////////////////////////////
+const whiteList = [
+  'duration',
+  'ratingsQuantity',
+  'ratingsAverage',
+  'maxGroupSize',
+  'difficulty',
+  'price',
+];
+///////////////////////////////
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+////////////////////////////////
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+const loginLimiter = rateLimit({
+  max: 5,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many login attempts from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+app.use('/api/v1/users/login', loginLimiter);
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(
+  hpp({
+    whiteList,
+  })
+);
+app.use(csurf());
 ////////////////////////////////
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
